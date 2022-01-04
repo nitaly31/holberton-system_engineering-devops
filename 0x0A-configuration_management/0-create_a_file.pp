83b5a49612b3041c2b111e1f32615ceb5ab67f84
nitaly31 <geraldine.nitaly@gmail.com>
2022-01-03 17:53:05 -0800

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Crea un archivo
 
-file{ '/tmp/school':
-  ensure => file,
-  path => '/tmp/school',
-  owner => www-data,
-  group => www-data,
-  mode => 0744,
-  content => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
