637b441832be8b0f94bccda0f8a87bb807b3b69b
nitaly31 <geraldine.nitaly@gmail.com>
2022-01-03 18:14:15 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
 # Instalar un paquete
 
 package { 'puppet-lint':
-  name     => 'puppet-lint',
   ensure   => '2.5.0',
+  name     => 'puppet-lint',
   provider => 'gem',
   source   => 'http://rubygems.org',
 }
