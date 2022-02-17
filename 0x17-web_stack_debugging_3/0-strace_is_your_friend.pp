e4ac1242cb380e30d9f91f9b1087c6d6cdfae5ee
nitaly31 <geraldine.nitaly@gmail.com>
2022-02-16 20:43:28 -0500

    500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix 500 error when a GET HTTP method is requested to Apache web server
-
-exec {'replace':
-  provider => shell,
-    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-    }
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+    command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      }
