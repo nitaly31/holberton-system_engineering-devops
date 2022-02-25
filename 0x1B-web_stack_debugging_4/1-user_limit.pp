91425118db9faad88a4778c6b95a0a15fc42730d
nitaly31 <geraldine.nitaly@gmail.com>
2022-02-24 19:02:30 -0500

    # Fix problem of high amount files opened

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Fix problem of high amount files opened
+
+exec {'Updating limits to Holberton User':
+  command  => "sed -i 's/holberton soft nofile.*/holberton soft nofile 9000/' /etc/security/limits.conf",
+  provider => shell
+}
+exec {'Updating limit to Holberton User':
+  command  => "sed -i 's/holberton hard nofile.*/holberton hard nofile 9000/' /etc/security/limits.conf",
+  provider => shell
+}
